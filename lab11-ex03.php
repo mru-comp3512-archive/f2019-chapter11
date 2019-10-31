74f91acf0134f6a5b2654c22afb1ce895f70b18d
Randy Connolly <rconnolly@mtroyal.ca>
2019-10-30 18:13:07 -0600

    first upload

---
+++ lab11-ex03.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Lab 11</title>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">  
+<link rel="stylesheet" href="css/lab11-ex03.css">    
+</head>
+<body>
+<h1>Age calculator</h1>
+
+<ul>
+   <li><?php  ?> seconds, or </li>
+   <li><?php  ?> days, or </li>
+   <li><?php  ?> months, or </li>
+   <li><?php  ?> years</li>
+</ul>
+</body>
+</html>
---
+++ lab11-ex05.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Lab 11</title>
+<style>
+   img { float:left; width: 132px; display: block; margin-right: 10px;}
+   h2 { margin: 0; font-size: 1.25em;}
+</style>
+</head>
+<body>
+<?php
+$thumbnail = "120010.jpg";
+$title = "Portrait of Eleanor of Toledo";
+
+$year = 1500;
+$era = "15th Century";  
+
+     
+?> 
+
+<h2><?php echo $era; ?></h2>
+
+
+</body>
+</html>
